df190cd2a0c63b7392b2ec4462f64208468ca53b
adaschuler <adaschuler@gmail.com>
2021-05-26 02:08:50 +0000

    ejercicio resuelto

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  var aleatorio4 = Math.floor(Math.random() * 4);
+  var aleatorio3 = Math.floor(Math.random() * 3);
+  var aleatorio5 = Math.floor(Math.random() * 5);
+  var excusa =
+    who[aleatorio4] +
+    " " +
+    action[aleatorio4] +
+    " " +
+    what[aleatorio3] +
+    " " +
+    when[aleatorio5];
+  let parrafo = this.document.getElementById("excuse");
+  parrafo.innerHTML = excusa;
 };
